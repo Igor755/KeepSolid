c9b607628492d1a2bd2b74f962c668a1e27b0bec
Sonikpalms <metlin.igor@gmail.com>
2017-07-17 23:38:15 +0300

    Lesson3

--- app/src/main/java/com/sonikpalms/intern/MainActivity.java
+++ app/src/main/java/com/sonikpalms/intern/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sonikpalms.intern;
 
+import android.app.usage.UsageEvents;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.data;
+import static android.R.attr.name;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textView;
+    private Button sendButton, clearButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +31,66 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        textView = (TextView) findViewById(R.id.editText);
+        sendButton = (Button) findViewById(R.id.sendButton);
+        clearButton = (Button) findViewById(R.id.clearButton);
+
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+                if (textView.getText().toString().equals("")) {
+
+                    Toast toastEmptyText = Toast.makeText(getApplicationContext(),
+                            "Please Enter text!", Toast.LENGTH_SHORT);
+                    toastEmptyText.show();
+
+                } else {
+                    Intent intent = new Intent(view.getContext(), Receiver.class);
+                    intent.putExtra("name", textView.getText().toString());
+                    startActivityForResult(intent, 1);
+                }
+
             }
+
         });
+
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textView.getText().toString().equals("")) {
+
+
+                    Toast toastEmptyText = Toast.makeText(getApplicationContext(),
+                            "TextView is Empty!", Toast.LENGTH_SHORT);
+                    toastEmptyText.show();
+
+
+                } else {
+                    textView.setText("");
+
+                }
+
+
+            }
+
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        String name = data.getStringExtra("name");
+        textView.setText(name);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +113,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/sonikpalms/intern/Receiver.java
@@ -0,0 +1,77 @@
+package com.sonikpalms.intern;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Receiver extends AppCompatActivity {
+
+
+    private TextView static_text;
+    private Button acceptButton, rejectButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        static_text = (TextView) findViewById(R.id.static_text);
+        acceptButton = (Button) findViewById(R.id.accept_button);
+        rejectButton = (Button) findViewById(R.id.reject_button);
+
+        // Принимаем имя
+        String txtName = getIntent().getStringExtra("name");
+        static_text.setText(static_text.getText().toString() + " " + txtName);
+
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(view.getContext(), MainActivity.class);
+                startActivity(intent);
+                Toast toastAccept = Toast.makeText(getApplicationContext(),
+                        "Accept!", Toast.LENGTH_SHORT);
+
+
+                toastAccept.show();
+
+            }
+
+        });
+        rejectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent();
+                intent.putExtra("name", static_text.getText().toString());
+                setResult(RESULT_OK, intent);
+
+
+
+                finish();
+                Toast toastReject = Toast.makeText(getApplicationContext(),
+                        "Reject!", Toast.LENGTH_SHORT);
+                toastReject.show();
+
+
+
+            }
+        });
+
+
+    }
+
+}
