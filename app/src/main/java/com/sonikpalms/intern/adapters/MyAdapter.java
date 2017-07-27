9b0e4444dacf6cb93c0a52b314384ddd609b6d75
i.metlin <metlin.igor@gmail.com>
2017-07-27 12:41:38 +0300

    Lesson5

---
+++ app/src/main/java/com/sonikpalms/intern/adapters/MyAdapter.java
@@ -0,0 +1,10 @@
+package com.sonikpalms.intern.adapters;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by i.metlin on 27.07.2017.
+ */
+
+public class MyAdapter extends BaseAdapter {
+}
--- app/src/main/java/com/sonikpalms/intern/com/sonikpalms/fragment/FragmentButton.java
+++ app/src/main/java/com/sonikpalms/intern/com/sonikpalms/fragment/FragmentButton.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +20,6 @@ import com.sonikpalms.intern.MainActivity;
 
 
 public class FragmentButton extends Fragment {
-
-
-    private TextView TextView1;
-    private Button buttonSend, buttonClear;
 
 
     @Override
@@ -36,51 +33,9 @@ public class FragmentButton extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_button, container, false);
 
-        Button buttonSend = (Button) v.findViewById(R.id.sendButton);
-        Button buttonClear = (Button) v.findViewById(R.id.clearButton);
-        final TextView TextView1 = (TextView) v.findViewById(R.id.editText);
-
-
-        buttonSend.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                if (TextView1.getText().toString().equals("")) {
-
-                    Toast toastEmptyText = Toast.makeText(getActivity().getApplicationContext(),
-                            "Please Enter text!", Toast.LENGTH_SHORT);
-                    toastEmptyText.show();
-
-                } else {
-
-                    Intent intent = new Intent(v.getContext(), Receiver.class);
-                    intent.putExtra("name", TextView1.getText().toString());
-                    startActivityForResult(intent, 1);
-                }
-            }
-        });
-
-
-        buttonClear.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                if (TextView1.getText().toString().equals("")) {
-
-
-                    Toast toastEmptyText = Toast.makeText(getActivity().getApplicationContext(),
-                            "TextView is Empty!", Toast.LENGTH_SHORT);
-                    toastEmptyText.show();
-
-
-                } else {
-
-
-                    TextView1.setText("");
-
-                }
-
-            }
-        });
+        ListView listView = (ListView) v.findViewById(R.id.list_item);
 
         return v;
     }
+
 }
